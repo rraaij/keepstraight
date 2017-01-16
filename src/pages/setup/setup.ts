e659918a75851c0c17e3050dd0033d2df091fed1
Ramon van Raaij <ramon.van.raaij@gmail.com>
2017-01-16 07:35:58 +0100

    added game and setup pages

---
+++ src/pages/setup/setup.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController} from 'ionic-angular';
+
+@Component({
+    selector: 'page-setup',
+    templateUrl: 'setup.html'
+})
+export class SetupPage {
+    constructor(public nav: NavController) {
+        
+    }
+}
