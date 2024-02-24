d639dbabbe36cee2cace25bfe12da9d73bba42b5
Ramon van Raaij <ramon.van.raaij@gmail.com>
2024-02-24 15:56:07 +0100

    scoretable added and styled

--- components/ScoreTable.tsx
+++ components/ScoreTable.tsx
@@ -1,11 +1,47 @@
-import { View, StyleSheet, FlatList } from "react-native";
-import { PlayerEnum } from "../lib/game.model";
+import { FlatList, StyleSheet, View, Text } from "react-native";
+import { PlayerEnum, Score } from "../lib/game.model";
 import { Colors } from "../lib/Colors";
+import { SCORES } from "@assets/SCORES";
 
 const ScoreTable = ({ player }: { player: PlayerEnum }) => {
+  const scores = SCORES.filter((score) => score.player === player);
+
+  const renderScore = ({ item }: { item: Score }) => {
+    return (
+      <View style={styles.scoreContainer}>
+        <Text style={[styles.datafield, { textAlign: "left" }]}>
+          {item.inning}
+        </Text>
+        <Text style={styles.datafield}>{item.score}</Text>
+        <Text style={[styles.datafield, { textAlign: "center" }]}>
+          {item.foul && <Text style={styles.foul}> F </Text>}
+        </Text>
+        <Text style={styles.datafield}>{item.total}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      {/*<FlatList data={} renderItem={} />*/}
+      <View style={styles.scoreContainer}>
+        <Text
+          style={[styles.datafield, { fontWeight: "bold", textAlign: "left" }]}
+        >
+          #
+        </Text>
+        <Text style={[styles.datafield, { fontWeight: "bold" }]}>S</Text>
+        <Text
+          style={[
+            styles.datafield,
+            { fontWeight: "bold", textAlign: "center" },
+          ]}
+        >
+          F
+        </Text>
+        <Text style={[styles.datafield, { fontWeight: "bold" }]}>T</Text>
+      </View>
+
+      <FlatList data={scores} renderItem={renderScore} />
     </View>
   );
 };
@@ -20,5 +56,23 @@ const styles = StyleSheet.create({
     borderColor: Colors.blue600,
     width: "100%",
     backgroundColor: "white",
+  },
+  scoreContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    paddingVertical: 2,
+    paddingHorizontal: 8,
+  },
+  datafield: {
+    width: "25%",
+    textAlign: "right",
+    fontSize: 18,
+  },
+  foul: {
+    backgroundColor: "red",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 12,
   },
 });
